c1c7dd81ac69e69ca3966dcd0a057eb07018da52
Pav85 <pawel.werbowy@gmail.com>
2023-08-06 11:28:26 +0100

    Create robots.js

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham'
+    username: "Bret",
+    email: "sincere@april.biz",
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell'
+    username: "Antonette",
+    email: "shanna@melissa.tv",
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch'
+    username: "Samantha",
+    email: "nathan@yesenia.net",
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack'
+    username: "Karianne",
+    email: "julianne@kory.org",
+  },
+  {
+    id: 5,
+    name: 'Dennis Schultz'
+    username: "Leopoodo",
+    email: "karley@jasper.info",
+  },
+  {
+    id: 6,
+    name: 'Kurtis Weissnat'
+    username: "Elvyn",
+    email: "telly@billy.biz",
+  },
+  {
+    id: 7,
+    name: 'Nicholas Smith'
+    username: "Maxime",
+    email: "sherwood@rosamond.me",
+  },
+  {
+    id: 8,
+    name: 'Glenna Reichert'
+    username: "Delphine",
+    email: "chaim_mcdermot@dana.io",
+  },
+];
