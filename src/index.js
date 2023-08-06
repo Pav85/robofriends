b6901fa83714970a8b6ca79857d5e17232999252
Pav85 <pawel.werbowy@gmail.com>
2023-08-06 15:38:04 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import Card from "./Card";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 import { robots } from "./robots";
+import CardList from "./CardList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <CardList />
     <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
     <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
     <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
