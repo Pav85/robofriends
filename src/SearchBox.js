e2b62a243a4043e0d704dedb07fec11c5a5b164e
Pav85 <pawel.werbowy@gmail.com>
2023-08-07 12:36:56 +0100

    Update SearchBox.js

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const SearchBox = () => {
-  return <div>SearchBox</div>;
+  return (
+    <div className="pa2">
+      <input
+        type="search"
+        placeholder="search robots"
+        className="pa3 ba b--green bg-lightest-blue"
+      />
+    </div>
+  );
 };
 
 export default SearchBox;
