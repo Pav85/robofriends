0fde0deef50a00c419c5c95ed52ce4a7619f6201
Pav85 <pawel.werbowy@gmail.com>
2023-08-07 12:22:09 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import { robots } from "./robots";
-import CardList from "./CardList";
+// import { robots } from "./robots";
+// import CardList from "./CardList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
